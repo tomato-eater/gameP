2281ada98f5244e325b7a3fa1a5ec20754e77e09
tarogituser <ktc23a32a0007@edu.kyoto-tech.ac.jp>
2025-10-11 17:21:08 +0900

    no message

--- Project1/Project1/p1.cpp
+++ Project1/Project1/p1.cpp
@@ -1,5 +1,24 @@
 #include<Windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        //画面更新処理
+        return 0;
+
+    case WM_KEYDOWN:
+        //キー入力処理
+        return 0;
+    }
+    return 0;
+}
+
 int WINAPI WinMain
 (
     HINSTANCE hInstance,      // アプリケーションの識別番号
@@ -10,7 +29,7 @@ int WINAPI WinMain
 {   // ここにメインの処理を書く
     // 1. ウィンドウクラス登録
     WNDCLASS wc{};
-    //wc.lpfnWndProc = WindowProc;
+    wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = "GameWindow";
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
@@ -19,11 +38,26 @@ int WINAPI WinMain
     RegisterClass(&wc);
 
     // 2. ウィンドウ作成
-    HWND hwnd=CreateWindow
+    HWND hwnd = CreateWindow
     (
-
-    )
+        "GameWindow",
+        "My Game",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL, NULL,
+        hInstance,
+        NULL
+    );
+    ShowWindow(hwnd, nCmdShow);
 
     // 3. メッセージループ
+    MSG msg{};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
